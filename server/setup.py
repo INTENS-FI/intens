fe7302317906c461179026cb1365b9f352f7c7f9
Timo Korvola <Timo.Korvola@vtt.fi>
2018-10-30 18:03:59 +0200

    Add some more package info.

--- server/setup.py
+++ server/setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 setup(
     name="Simsvc",
     version="0.1",
+    author="Timo Korvola",
+    author_email="Timo.Korvola@vtt.fi",
+    description="A distributed simulation service",
     packages=["simsvc"],
     scripts=["run-server.py", "sio-test.py"],
     python_requires=">= 3.6",
     install_requires=["Flask", "Flask-SocketIO", "ZODB", "eventlet", 
-                      "dask", "distributed", "socketIO-client"])
+                      "dask", "distributed", "socketIO-client"],
+)
