3b4ba051811e354b8f19d44c588219e3b19e600e
Timo Korvola <Timo.Korvola@vtt.fi>
2018-11-13 18:35:01 +0200

    New test model.
    Launches a Python script as a subprocess, works also on Windows
    (unlike the Bourne shell script).  Uses Cityopt compatible (dotted)
    names.

---
+++ models/csum/__init__.py
@@ -0,0 +1,44 @@
+"""Another subprocess test model.
+Computes c.x + c.y (dotted names for Cityopt compatibility) with
+an external program using the only scripting language that we can trust
+to be available on every platform: Python.
+"""
+
+from concurrent.futures import CancelledError
+import sys, os, subprocess as sp, traceback as tb
+
+import dask
+
+def run_it(spec):
+    #TODO
+    script = os.path.join(os.path.dirname(__file__), "script.py")
+    return sp.Popen(
+        [sys.executable, script]
+        + [str(spec.inputs[n]) for n in ['c.x', 'c.y']],
+        cwd=spec.workdir, stdin=sp.DEVNULL, stdout=sp.PIPE, stderr=sp.PIPE,
+        universal_newlines=True)
+
+def main(spec, cancel):
+    out = err = None
+    with run_it(spec) as proc:
+        while True:
+            if cancel.get():
+                raise CancelledError("Cancelled by request")
+            try:
+                out, err = proc.communicate(timeout=5)
+            except sp.TimeoutExpired:
+                continue
+            if proc.returncode:
+                raise RuntimeError(
+                    ("Subprocess exited with status %s. stdout:\n%s\n"
+                     + "stderr:\n%s") % (proc.returncode, out, err))
+            else:
+                return {"c.sum": int(out), "c.warnings": err}
+
+@dask.delayed
+def task(spec, cancel):
+    try:
+        return main(spec, cancel)
+    except:
+        tb.print_exc()
+        raise
---
+++ models/csum/script.py
@@ -0,0 +1,6 @@
+if __name__ != '__main__':
+    raise RuntimeError("This is a script.  Run, don't import.")
+
+import sys, time
+time.sleep(5)
+print(sum(int(s) for s in sys.argv[1:]))
