8aa6261ac62c784759f9631d1fefba4c6b382dd3
Timo Korvola <Timo.Korvola@vtt.fi>
2018-11-20 13:56:19 +0200

    Update setup.py.
    Require the versions of distributed and tornado that have been
    succesfully tested with, or newer.  Drop sio-test.py.

--- server/setup.py
+++ server/setup.py
@@ -6,8 +6,8 @@ setup(
     author_email="Timo.Korvola@vtt.fi",
     description="A distributed simulation service",
     packages=["simsvc"],
-    scripts=["run-server.py", "sio-test.py"],
+    scripts=["run-server.py"],
     python_requires=">= 3.6",
     install_requires=["Flask", "Flask-SocketIO", "ZODB", "eventlet", 
-                      "dask", "distributed", "socketIO-client"],
+                      "dask", "distributed >= 1.24.1", "tornado >= 5.1.1"],
 )
