c87bb75386df38fe191f5564b5af376243c8fbb9
Timo Korvola <Timo.Korvola@vtt.fi>
2018-11-26 13:07:34 +0200

    Also set up cert-manager.
    No longer takes command line args.

--- etc/make-sscert.sh
+++ etc/make-sscert.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
-# Create a self-signed certificate and install as Kubernetes secret intens-tls.
-# Args are common name (CN) and organisation (O) for the cert.  The private key
-# and cert are stored in local files intens-tls.{key,pem}.  There is no
-# passphrase on the private key.
+# Create a self-signed CA certificate and install as Kubernetes secret.
+# Create a cert-manager cluster issuer that uses the new certificate.
+# The private key and cert are also stored in local files.
+# There is no passphrase on the private key.
 
-name=intens-tls
+# Certificate subject
+subj="/CN=Intens/O=VTT"
+# Name of the secret, also base name of generated files
+sname=intens-tls
+# Name of the cluster issuer
+ciname=intens-issuer
 
 set -e
 umask 77
 
-openssl req -x509 -nodes -newkey rsa:2048 \
-    -keyout ${name}.key -out ${name}.pem \
-    -reqexts v3_req -extensions v3_ca -subj "/CN=$1/O=$2"
+openssl genrsa -out ${sname}.key 2048
+openssl req -x509 -nodes -key ${sname}.key -out ${sname}.pem \
+    -reqexts v3_req -extensions v3_ca -subj "$subj"
 
-kubectl create secret tls $name --key ${name}.key --cert ${name}.pem
+kubectl create secret tls $sname --key ${sname}.key --cert ${sname}.pem
+
+kubectl apply -f - <<@end
+apiVersion: certmanager.k8s.io/v1alpha1
+kind: ClusterIssuer
+metadata:
+  name: $ciname
+spec:
+  ca:
+    secretName: $sname
+@end
