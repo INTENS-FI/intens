d88078dc3e81c0152f2c5935ba5644f562f15f73
Timo Korvola <Timo.Korvola@vtt.fi>
2019-04-15 14:32:54 +0300

    Shell script to clean up ACR.
    Removes untagged images.

---
+++ aks/acr-prune.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Delete all untagged images from ACR.
+# https://docs.microsoft.com/en-us/azure/container-registry/container-registry-delete#delete-untagged-images
+
+reg=intens
+
+for repo in `az acr repository list -n $reg -o tsv`
+do for img in `az acr repository show-manifests -n $reg --repository "$repo" \
+                   --query '[?tags[0] == null].digest' -o tsv`
+   do  echo "$repo@$img"
+       az acr repository delete -n $reg --image "$repo@$img" --yes
+   done
+done
