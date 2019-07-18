150ca14c57ad8fe01be1d3acd8f7e5eeaba71c04
Timo Korvola <Timo.Korvola@vtt.fi>
2019-07-18 15:27:41 +0300

    Modify oc-token.sh to use plain kubectl.
    oc-token.sh might still be useful for granting an external cluster
    such as Minikube access to the Rahti registry.  Modify the script
    to serve that purpose: configure with kubectl and only use oc for
    getting the token from a different (non-default) context.

--- csc/oc-token.sh
+++ csc/oc-token.sh
@@ -1,15 +1,20 @@
 #!/bin/sh -e
-# Set up an image pull secret using the OpenShift access token.
-# This lets Kubernetes pull from the registry directly.
-# The token changes frequently, forcing you to run this again.
-# It seems you don't need this for pulling image streams, thus it
-# is likely best to use those instead.
+# Set up an image pull secret using the OpenShift access token.  This
+# lets Kubernetes pull from the registry directly.  The token changes
+# frequently, forcing you to run this again.
+#
+# On OpenShift it seems you don't need this for pulling image streams
+# created by pushing to the integrated registry.  However, if you want
+# to access that registry from an external Kubernetes cluster, e.g.,
+# Minikube, this should help.
 
 sname=rahti-reg
 reg=docker-registry.rahti.csc.fi
 sa=default
+occontext=/rahti-csc-fi:8443/korvolat
 
-oc delete secret $sname || echo "Never mind that."
-oc create secret docker-registry $sname --docker-server=$reg \
-    --docker-username=unused --docker-password="`oc whoami -t`"
-oc secrets link --for=pull $sa $sname
+kubectl delete secret $sname || echo "Never mind that."
+kubectl create secret docker-registry $sname --docker-server=$reg \
+        --docker-username=unused \
+        --docker-password="`oc whoami -t --context=$occontext`"
+kubectl patch sa $sa -p '{"imagePullSecrets": [{"name": "'$sname'"}]}'
