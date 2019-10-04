bca86642c2ab49420a3f8f2a718550e5dc944fd1
Timo Korvola <Timo.Korvola@vtt.fi>
2019-10-04 13:48:13 +0300

    Support a setup script for the Simulink build.
    E.g., if the Simulink model references global variables, a setup
    script can load them from a .mat file.

--- simulink/build_rtw.m
+++ simulink/build_rtw.m
@@ -1,5 +1,10 @@
 % Build model given by env. var. SL_MODEL
 
+sup = getenv('MAT_SETUP');
+if ~isempty(sup)
+  run(sup);
+end
+
 [p, n, x] = fileparts(string(getenv('SL_MODEL')));
 if p ~= ""
   cd(p);
