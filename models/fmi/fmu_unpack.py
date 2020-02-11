f0bb74ddb89be11a7c7befc8308ef57fed23ca94
Timo Korvola <Timo.Korvola@vtt.fi>
2020-02-11 14:48:16 +0200

    Use dask.config also for FMU location.
    FMI support is not part of simsvc proper, so there won't be a default
    value in YAML.  Handle it in code.

--- models/fmi/fmu_unpack.py
+++ models/fmi/fmu_unpack.py
@@ -2,7 +2,7 @@
 """
 
 import os, tempfile, atexit, logging
-import fmpy
+import dask.config, fmpy
 
 tmp = fmu = None
 
@@ -15,12 +15,23 @@ def cleanup():
         tmp.cleanup()
     tmp = fmu = None
 
+def expand_path(f):
+    """Convert f to an absolute path.
+
+    f can be an absolute path or relative to the package directory.
+    The usual tilde constructions are expanded if possible.
+    """
+    return os.path.join(os.path.dirname(__file__),
+                        os.path.expanduser(f))
+
 def unpack_model():
     """Unpack the model into a temporary directory.
 
-    The model file is given by the environment variable MODEL_FMU
-    (full path), default model.fmu in the package directory.  This
-    sets module variables tmp and fmu to point at a newly created
+    The model file is given by the environment variable MODEL_FMU or
+    Dask config item simsvc.model.fmu.  Default is "model.fmu".  The
+    file name is processed with expand_path.
+
+    This sets module variables tmp and fmu to point at a newly created
     tempfile.TemporaryDirectory and the extracted fmu, respectively.
     It also registers an atexit hook to remove tmp.
     """
@@ -30,7 +41,9 @@ def unpack_model():
         tmp = tempfile.TemporaryDirectory()
         fmu = None
     if fmu is None:
-        f = os.getenv("MODEL_FMU") or os.path.join(os.path.dirname(__file__),
-                                                   "model.fmu")
-        fmu = fmpy.extract(f, tmp.name)
+        f = os.getenv("MODEL_FMU") or dask.config.get("simsvc.model.fmu",
+                                                      "model.fmu")
+        fmu = fmpy.extract(os.path.join(os.path.dirname(__file__),
+                                        os.path.expanduser(f)),
+                           tmp.name)
         logger.info("Extracted FMU to %s", fmu)
