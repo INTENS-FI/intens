0880dc8a0a18ffdb12c14d9fd5e9d55398a0aa9b
Timo Korvola <Timo.Korvola@vtt.fi>
2020-03-19 17:39:07 +0200

    Switch to the client that comes with python-socketio.
    Also add a verbose logging option.

--- server/sio-test.py
+++ server/sio-test.py
@@ -2,10 +2,10 @@
 
 import logging, argparse
 
-from socketIO_client import SocketIO
+import socketio
 
-logging.getLogger('socketIO-client').setLevel(logging.DEBUG)
 logging.basicConfig()
+logger = logging.getLogger(__name__)
 
 def on_launched(jid):
     print("Launched", jid)
@@ -19,12 +19,21 @@ if __name__ == '__main__':
                    help="Root CA bundle for SSL verification")
     p.add_argument('-t', '--trust', action='store_true',
                    help="Disable SSL certificate verification")
+    p.add_argument('-v', '--verbose', action='count',
+                   help="Enable Engine.IO logging.  -vv for debug level.")
     p.add_argument(
         'url', nargs='?', default="http://localhost:8080",
         help="Simsvc base URL (without trailing slash, default %(default)s)")
     args = p.parse_args()
     verify = False if args.trust else (args.cafile or True)
-    with SocketIO(args.url, verify=verify) as sioc:
-        sioc.on('launched', on_launched)
-        sioc.on('terminated', on_terminated)
-        sioc.wait()
+    if args.verbose:
+        eio_log = logger
+        if args.verbose > 1:
+            logger.setLevel(logging.DEBUG)
+    else:
+        eio_log = False
+    sioc = socketio.Client(ssl_verify=verify, engineio_logger=eio_log)
+    sioc.on('launched', on_launched)
+    sioc.on('terminated', on_terminated)
+    sioc.connect(args.url)
+    sioc.wait()
