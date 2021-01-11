3fec43fdd09cf45361d98ee2b94edee409f8bbe6
Hannu Rummukainen <Hannu.Rummukainen@vtt.fi>
2021-01-11 17:14:49 +0200

    Extra requirements for Bayesian optimisation [bo].

--- python_client/setup.py
+++ python_client/setup.py
@@ -9,5 +9,6 @@ setup(
     author_email='hannu.rummukainen@vtt.fi',
     keywords=['simulation'],
     python_requires=">= 3.6",
-    install_requires=["requests", "python-socketio", "numpy"]
+    install_requires=["requests", "python-socketio", "numpy"],
+    extras_require={"bo": ["dragonfly", "pandas", "pyyaml"]}
 )
