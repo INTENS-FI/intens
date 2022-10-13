fa3618ef4c9a068fd0f8c60c091da6ab3fd823e3
Timo Korvola <Timo.Korvola@vtt.fi>
2022-10-13 10:15:45 +0300

    Move everything from setup.py to setup.cfg.

--- server/setup.py
+++ server/setup.py
@@ -1,15 +1,2 @@
 from setuptools import setup
-setup(
-    name="Simsvc",
-    version="0.5.3",
-    author="Timo Korvola",
-    author_email="Timo.Korvola@vtt.fi",
-    description="A distributed simulation service",
-    packages=["simsvc"],
-    include_package_data=True,
-    python_requires=">= 3.6",
-    install_requires=["Flask", "Flask-SocketIO", "ZODB", "zodburi",
-                      "dask", "distributed >= 1.25.0", "tornado >= 5.1.1",
-                      "eventlet", "flask_httpauth", "passlib >= 1.7",
-                      "pyyaml"],
-)
+setup()
